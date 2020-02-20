180edefcc7e4d9bdd1008597063ff0fe847adc12
jonathanprange <jonathanprange8000@gmail.com>
2020-02-20 00:46:56 -0300

    aula 5

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
     # Introdução
     # 1. teste_assert.py
 # Aula 2
-    # 2. teste_sem_framework.py
+    # 2. teste_sem_framework.py (recaptular métodos e classes)
 # Aula 3
     # 3. teste_com_framework.py
     # 3.1 tests/test_com_framework.py
@@ -13,3 +13,12 @@
     # 4.2 tdd_2.py e tests/test_tdd_2.py 
     # 4.3 tdd_3.py e tests/test_tdd_3.py 
     # 4.4 tdd_4.py e tests/test_tdd_4.py 
+
+# (mock/mock.py) Arquivo em comum para as aulas 5, 6 e 7.
+
+
+# Aula 5
+    # 5. mock/mock.py e tests/test_cobertura.py
+
+# Aula 6
+    # 6. mock/mock.py e testes/test_mock_1.py
---
+++ app/mock/mock.py
@@ -0,0 +1,64 @@
+# Iremos nos aprofundar um pouco mais nos testes unitários, garantindo que realmente estamos testando uma unidade por vez.
+
+# Vamos simular a vendinha do Zé, ele terá alguns produtos para comercializar, onde podemos escolher um produto
+# e calcular o valor total da compra.
+
+
+class VendinhaDoZe:
+# Temos aqui a classe da vendinha do Zé
+    def __init__(self):
+        self.salgado = ''
+        self.quantidade_de_salgados = 0
+        self.valor_do_salgado = 3.50
+        self.refrigerante = ''
+        self.quantidade_de_refrigerantes = 0
+        self.valor_do_refrigerante = 2.50
+    # Temos aqui alguns produtos que o a vendinha do Zé comercializa
+
+    
+    def realizar_pedido(self):
+    # Temos aqui um método que coletará do cliente, as informações para montar um pedido
+        self.salgado = input('Informe um salgado [coxinha/pastel/enroladinho]: ')
+        self.quantidade_de_salgados = input('Informe quantas unidades de salgado: ')
+        self.refrigerante = input('Informe um refrigerante [pepsi/sukita/guaraná]: ')
+        self.quantidade_de_refrigerantes = input('Informe quantas unidades de refrigerante: ')
+        # Temos aqui os input's para coletarmos as informações de nossos clientes
+
+
+    def imprime_pedido(self):
+        # Temos aqui um método que imprime o pedido realizado pelo cliente, para isso ele chama outros dois
+        # métodos da classe, para receber os valores totais para imprimir
+        valor_total_dos_refrigerantes = self.calculo_valor_total_refrigerantes()
+        valor_total_dos_salgados = self.calculo_valor_total_salgados()
+        valor_total_da_compra = self.calculo_valor_total_da_compra(valor_total_dos_salgados, valor_total_dos_refrigerantes)
+        print('Seu pedido é: ')
+        print(f'{self.quantidade_de_salgados} unidades de {self.salgado} no valor de {valor_total_dos_salgados}')
+        print(f'{self.quantidade_de_refrigerantes} unidades de {self.refrigerante} no valor de {valor_total_dos_refrigerantes}')
+        print(f'O valor total da compra é de R$ {valor_total_da_compra}')
+
+
+    def calculo_valor_total_da_compra(self, valor_total_dos_salgados, valor_total_dos_refrigerantes):
+    # Temos aqui um método que recebe o valor total de salgados, o valor total de refrigerantes, calcula e retorna 
+    # o valor total da compra.
+        valor_total_da_compra = valor_total_dos_salgados + valor_total_dos_refrigerantes
+        return valor_total_da_compra
+
+
+    def calculo_valor_total_salgados(self):
+    # Temos aqui um método que calcula o valor total dos salgados solicitados pelo cliente e retorna esse valor
+        valor_total_dos_salgados = self.quantidade_de_salgados * self.valor_do_salgado
+        return valor_total_dos_salgados
+    
+
+    def calculo_valor_total_refrigerantes(self):
+    # Temos aqui um método que calcula o valor total dos refrigerantes solicitados pelo cliente e retorna esse valor
+        valor_total_dos_refrigerantes = self.quantidade_de_refrigerantes * self.valor_do_refrigerante
+        return valor_total_dos_refrigerantes
+
+
+# Temos aqui uma classe completa, com diversos atributos e métodos, vamos testar cada um deles individualmente
+# não será necessário criarmos todo nosso programa para garantirmos de cada funcionalidade da nossa classe, 
+# com o teste unitário e o mock, vamos testar e simular cada comportamento isoladamente.
+
+
+# Vamos começar com o test_mock_1.py dentro da pasta tests
